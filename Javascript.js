c97dd14451030489f8335673fe740aa2387a34e1
Rupsje3 <stijn.goedemans@icloud.com>
2026-06-08 09:23:57 +0200

    Update Javascript.js

--- Javascript.js
+++ Javascript.js
@@ -11,27 +11,40 @@ const keuzes = ["Steen", "Papier", "Schaar"];
 
 // Functie om te spelen
 function spelen(spelerKeuze) {
-}
 
-// Computer kiest willekeurig
-let computerKeuze = keuzes[Math.floor(Math.random() * 3)];
+    // Computer kiest willekeurig
+    let computerKeuze = keuzes[Math.floor(Math.random() * 3)];
+
+    // resultaten tonen
+    document.getElementById("speler-keuze").innerHTML = spelerKeuze;
+    document.getElementById("computer-keuze").innerHTML = computerKeuze;
+
+    let resultaat = "";
+
+    //winnaar bepalen
+    if (spelerKeuze === computerKeuze) {
+        resultaat.textContent = "Gelijkspel!";
+    }
+    else if ((spelerKeuze === "Steen" && computerKeuze === "Schaar") ||
+            (spelerKeuze === "Papier" && computerKeuze === "Steen") ||
+            (spelerKeuze === "Schaar" && computerKeuze === "Papier")) {
+        resultaat.textContent = "Je wint!";
+    } else {
+        const resultaatElement = document.getElementById("resultaat");
+    }
+
+    return{
+        spelerKeuze: spelerKeuze,
+        computerKeuze: computerKeuze,
+        resultaat: resultaat
+    };
+
+    console.log("Speler keuze: " + spelerKeuze);
+    console.log("Computer keuze: " + computerKeuze);
+    console.log("Resultaat: " + resultaat.textContent);
+}
 
 // event listeners toevoegen aan knoppen
 steenKnop.addEventListener("click", () => spelen("Steen"));
 papierKnop.addEventListener("click", () => spelen("Papier"));
 schaarKnop.addEventListener("click", () => spelen("Schaar")); 
-
-if (spelerKeuze === computerKeuze) {
-    resultaat.textContent = "Gelijkspel!";
-}
-else if ((spelerKeuze === "Steen" && computerKeuze === "Schaar") ||
-         (spelerKeuze === "Papier" && computerKeuze === "Steen") ||
-         (spelerKeuze === "Schaar" && computerKeuze === "Papier")) {
-    resultaat.textContent = "Je wint!";
-} else {
-    resultaat.textContent = "De computer wint!";
-}
-
-return spelerKeuze;
-return computerKeuze;
-return resultaat;
